64eb5a144547de95b2caca26fbae53a4c499c03c
Thomas Meitz <info@jproxx.com>
2025-06-16 11:06:50 +0200

    updated comments

--- schema/config.go
+++ schema/config.go
@@ -4,21 +4,24 @@ import (
 	"github.com/hashicorp/hcl/v2/hclsimple"
 )
 
+// Config represents the configuration structure for the Maschine plugin system.
 type Config struct {
 	Maschine MaschineBlock `hcl:"maschine,block"`
 }
 
+// MaschineBlock represents the top-level block in the configuration file.
 type MaschineBlock struct {
 	Plugins []PluginBlock `hcl:"plugin,block"`
 }
 
+// PluginBlock represents a plugin configuration block within the Maschine block.
 type PluginBlock struct {
 	Name    string `hcl:",label"`
 	Source  string `hcl:"source"`
 	Version string `hcl:"version"`
 }
 
-// Beispiel, wie man ein solches HCL-File parsen könnte:
+// LoadConfig loads a configuration from the specified HCL file.
 func LoadConfig(filename string) (*Config, error) {
 	var cfg Config
 	err := hclsimple.DecodeFile(filename, nil, &cfg)
